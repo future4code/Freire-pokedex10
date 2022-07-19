faf7925d06b6821d00c05041c5fe14c9a3e518a7
Andrei <123andrei.santos@gmail.com>
2022-07-19 16:46:31 -0300

    header-ajuste

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,16 +1,16 @@
-import React, {useState} from 'react';
-import styled from 'styled-components';
-import axios from 'axios';
-import { Router } from './Routes/Router';
-import { GlobalStyle } from './Global';
-import Header from './components/Header/Header';
+import React, { useState } from "react";
+import styled from "styled-components";
+import axios from "axios";
+import { Router } from "./Routes/Router";
+import { GlobalStyle } from "./Global";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div>
-      <Router/>
-      <Header/>
-      <GlobalStyle/>
+      <Header />
+      <Router />
+      <GlobalStyle />
     </div>
   );
 }
--- pokedex/src/components/Header/style.js
+++ pokedex/src/components/Header/style.js
@@ -1,14 +1,9 @@
 import styled from "styled-components";
 
 export const HeaderContainer = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    position: absolute;
-
-    width: 100%;
-    height: 160px;
-    left: 0px;
-    top: 0px;
-    background-color: white;
-`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 160px;
+  background-color: white;
+`;
