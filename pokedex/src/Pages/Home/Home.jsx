992c874761bbb9cdc8c17842f6331bfcd6320549
carolfortuna <63572811+carolfortuna@users.noreply.github.com>
2022-07-19 16:27:52 -0300

    card estático estilizado

--- pokedex/src/Pages/Home/Home.jsx
+++ pokedex/src/Pages/Home/Home.jsx
@@ -2,19 +2,14 @@ import React, {useState} from 'react';
 import styled from 'styled-components';
 import axios from "axios"
 import {useNavigate} from "react-router-dom"
-import PokemonCard from '../../components/PokemonCard'
-import bulbasaur from '../../components/PokemonCard/img/bulbasaur.png'
+import { PokemonCard } from '../../components/PokemonCard/PokemonCard';
 
 function Home() {
   return (
     <div>
-    <PokemonCard>
-    <p>01</p>
-    <h1>Bulbasaur</h1>
-<img src={bulbasaur} alt="" />
-
-    </PokemonCard>
-    Home
+    
+    <PokemonCard></PokemonCard>
+  
     </div>
   )
 }
--- pokedex/src/components/PokemonCard/PokemonCard.jsx
+++ pokedex/src/components/PokemonCard/PokemonCard.jsx
@@ -1,98 +1,27 @@
+import {Card, PhotoContainer, Background, ButtonType, DetalhesContainer, ButtonDiv} from './styles'
+import bulbasaur from './img/bulbasaur.png'
+import background from '../../components/PokemonCard/img/pokebola-bg.png'
+import folha from './img/folha.png'
 
-import styled from 'styled-components';
-import background from './img/pokebola-bg.png'
+export const PokemonCard =()=>{
 
-export const PokemonCard = styled.div`
-position: absolute;
-width: 440px;
-height: 210px;
-left: 0px;
-top: 0px;
-
-background: #729F92;
-border-radius: 12px;
-
-
-`
-
-export const PhotoContainer = styled.div `
-position: absolute;
-width: 210.73px;
-height: 210.73px;
-left: 294.37px;
-top: -62px;
-
-background: {background};
-opacity: 0.17;
-transform: rotate(30deg);
-
-& img{
-    
-position: absolute;
-width: 193px;
-height: 193px;
-left: 274px;
-top: 295px;
+    return (
+      <div>
+          <Card>
+    <p>#01</p>
+    <h1>Bulbasaur</h1>
+    {/* <img src={background} alt="" /> */}
+    <PhotoContainer>
+    <img src={bulbasaur} alt="" />
+    </PhotoContainer>
+    <ButtonDiv>
+    <ButtonType> <img src={folha} alt="" /> Grass </ButtonType>
+    <ButtonType> <img src={folha} alt="" /> Grass </ButtonType>
+    </ButtonDiv>
+    <DetalhesContainer>
+      <p>Detalhes</p> <button>Excluir</button>
+    </DetalhesContainer>
+    </Card>
+    </div>
+    )
 }
-`
-
-export const  ButtonType = styled.button`
-display: flex;
-flex-direction: row;
-align-items: flex-start;
-padding: 5px 8px;
-gap: 17px;
-
-position: absolute;
-width: 91px;
-height: 31px;
-left: 129px;
-top: 89px;
-
-background: #70B873;
-border: 1px dashed rgba(255, 255, 255, 0.47);
-border-radius: 8px;
-& img{
-    width: 17px;
-height: 19px;
-
-background: #FFFFFF;
-}
-`
-
-export const DetalhesContainer = styled.div`
-display: flex;
-flex-direction: row;
-justify-content: space-between;
-align-items: center;
-padding: 0px;
-gap: 135px;
-& p{
-    width: 74px;
-height: 24px;
-
-font-family: 'Poppins';
-font-style: normal;
-font-weight: 700;
-font-size: 16px;
-line-height: 24px;
-/* identical to box height */
-
-text-decoration-line: underline;
-
-color: #FFFFFF;
-& button {
-    display: flex;
-flex-direction: row;
-justify-content: center;
-align-items: center;
-padding: 4px 10px;
-
-width: 146px;
-height: 38px;
-
-background: #FF6262;
-border-radius: 8px;
-}
-}
-`
