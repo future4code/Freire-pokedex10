57d5d7d1a019e6edb47de7a722f592a48470f43d
Andrei <123andrei.santos@gmail.com>
2022-07-19 15:20:45 -0300

    header

--- pokedex/src/Pages/Home/Home.jsx
+++ pokedex/src/Pages/Home/Home.jsx
@@ -1,22 +1,21 @@
-import React, {useState} from 'react';
-import styled from 'styled-components';
-import axios from "axios"
-import {useNavigate} from "react-router-dom"
-import PokemonCard from '../../components/PokemonCard'
-import bulbasaur from '../../components/PokemonCard/img/bulbasaur.png'
+import React, { useState } from "react";
+import styled from "styled-components";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+// import PokemonCard from '../../components/PokemonCard'
+import bulbasaur from "../../components/PokemonCard/img/bulbasaur.png";
 
 function Home() {
   return (
     <div>
-    <PokemonCard>
-    <p>01</p>
-    <h1>Bulbasaur</h1>
-<img src={bulbasaur} alt="" />
-
-    </PokemonCard>
-    Home
+      {/* <PokemonCard> */}
+      <p>01</p>
+      <h1>Bulbasaur</h1>
+      <img src={bulbasaur} alt="" />
+      {/* </PokemonCard> */}
+      Home
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
