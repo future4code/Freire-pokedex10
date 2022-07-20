dfeaeba5b1eadbe7ac547573c1f9216cf83f84f1
Andrei <123andrei.santos@gmail.com>
2022-07-20 14:00:24 -0300

    detalhes

---
+++ pokedex/src/Pages/Details/style.js
@@ -0,0 +1,122 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  color: white;
+  font-family: sans-serif;
+
+  h1 {
+    max-width: 200px;
+    margin-left: 30px;
+
+    @media (max-width: 650px) {
+      font-size: 18px;
+    }
+  }
+
+  width: 100%;
+  max-width: 1200px;
+  margin: auto;
+`;
+
+export const ContainerArea = styled.div`
+  position: relative;
+  height: 0px;
+  padding: 0px 0px 47.72%;
+  margin-left: 20px;
+  margin-right: 20px;
+`;
+
+export const Card = styled.div`
+  position: Absolute;
+  top: 0px;
+  bottom: 0px;
+  left: 0px;
+  width: 100%;
+  height: 100%;
+  background-color: #729f92;
+  margin-bottom: 50px;
+  border-radius: 2vw;
+
+  #front {
+    background-color: white;
+    width: 20%;
+    position: absolute;
+    bottom: 50%;
+    top: 4%;
+    left: 2%;
+    margin-bottom: 15px;
+    display: flex;
+    align-items: center;
+    border-radius: 1vw;
+
+    img {
+      width: 100%;
+    }
+  }
+
+  #back {
+    background-color: white;
+    width: 20%;
+    position: absolute;
+    bottom: 4%;
+    top: 50%;
+    left: 2%;
+    margin-top: 15px;
+    display: flex;
+    align-items: center;
+    border-radius: 1vw;
+
+    img {
+      width: 100%;
+    }
+  }
+
+  #stats {
+    background-color: white;
+    width: 25%;
+    position: absolute;
+    left: 24%;
+    top: 4%;
+    bottom: 4%;
+    border-radius: 1.3vw;
+
+    img {
+      width: 100%;
+      border-radius: 10px;
+    }
+  }
+
+  #title {
+    width: 20%;
+    position: absolute;
+    bottom: 80%;
+    top: 4%;
+    left: 52%;
+    background-color: white;
+    border-radius: 1vw;
+  }
+
+  #moves {
+    background-color: white;
+    width: 20%;
+    position: absolute;
+    bottom: 4%;
+    top: 27%;
+    left: 52%;
+    border-radius: 1.3vw;
+  }
+
+  #image {
+    width: 20%;
+    position: absolute;
+    bottom: 80%;
+    top: -17%;
+    left: 77%;
+    display: flex;
+    align-items: center;
+
+    img {
+      width: 100%;
+    }
+  }
+`;
--- pokedex/src/components/PokemonCard/PokemonCard.jsx
+++ pokedex/src/components/PokemonCard/PokemonCard.jsx
@@ -1,27 +1,39 @@
-import {Card, PhotoContainer, Background, ButtonType, DetalhesContainer, ButtonDiv} from './styles'
-import bulbasaur from './img/bulbasaur.png'
-import background from '../../components/PokemonCard/img/pokebola-bg.png'
-import folha from './img/folha.png'
+import {
+  Card,
+  PhotoContainer,
+  Background,
+  ButtonType,
+  DetalhesContainer,
+  ButtonDiv,
+} from "./styles";
+import bulbasaur from "./img/bulbasaur.png";
+import background from "../../components/PokemonCard/img/pokebola-bg.png";
+import folha from "./img/folha.png";
 
-export const PokemonCard =()=>{
-
-    return (
-      <div>
-          <Card>
-    <p>#01</p>
-    <h1>Bulbasaur</h1>
-    {/* <img src={background} alt="" /> */}
-    <PhotoContainer>
-    <img src={bulbasaur} alt="" />
-    </PhotoContainer>
-    <ButtonDiv>
-    <ButtonType> <img src={folha} alt="" /> Grass </ButtonType>
-    <ButtonType> <img src={folha} alt="" /> Grass </ButtonType>
-    </ButtonDiv>
-    <DetalhesContainer>
-      <p>Detalhes</p> <button>Excluir</button>
-    </DetalhesContainer>
-    </Card>
+export const PokemonCard = () => {
+  return (
+    <div>
+      <Card>
+        <p>#01</p>
+        <h1>Bulbasaur</h1>
+        {/* <img src={background} alt="" /> */}
+        <PhotoContainer>
+          <img src={bulbasaur} alt="" />
+        </PhotoContainer>
+        <ButtonDiv>
+          <ButtonType>
+            {" "}
+            <img src={folha} alt="" /> Grass{" "}
+          </ButtonType>
+          <ButtonType>
+            {" "}
+            <img src={folha} alt="" /> Grass{" "}
+          </ButtonType>
+        </ButtonDiv>
+        <DetalhesContainer>
+          <p>Detalhes</p> <button>Excluir</button>
+        </DetalhesContainer>
+      </Card>
     </div>
-    )
-}
+  );
+};
