587b842f6f5fa53e076a34061041ade370afac0d
Andrei <123andrei.santos@gmail.com>
2022-07-20 14:30:12 -0300

    header

--- pokedex/src/components/Header/Header.jsx
+++ pokedex/src/components/Header/Header.jsx
@@ -1,26 +1,25 @@
-import React, {useState} from 'react'
-import PokeLogo from '../../img/logo.png'
-import { HeaderContainer } from './style'
-import { Button } from './style'
+import React, { useState } from "react";
+import PokeLogo from "../../img/logo.png";
+import { HeaderContainer } from "./style";
+import { Button } from "./style";
 
 export default function Header() {
-
   const [screen, setScreen] = useState("/");
 
-    // getHeaderPage = () => {
-    //   switch(){
-    //     case "":
-    //       return 
-    //   }
-    // }
+  // getHeaderPage = () => {
+  //   switch(){
+  //     case "":
+  //       return
+  //   }
+  // }
 
-    console.log(window.location.pathname)
+  console.log(window.location.pathname);
 
   return (
     <HeaderContainer>
-      <button></button>
+      <Button>Pokédex</Button>
       <img src={PokeLogo}></img>
       <Button>Pokédex</Button>
     </HeaderContainer>
-  )
+  );
 }
