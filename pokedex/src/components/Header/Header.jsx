1166bc88a610fe11e23868b672ba95528caff94c
Brenno Boechat <BrennoBoechat81@hotmail.com>
2022-07-20 13:41:53 -0300

    arrumando header

--- pokedex/src/components/Header/Header.jsx
+++ pokedex/src/components/Header/Header.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, {useState} from 'react'
 import PokeLogo from '../../img/logo.png'
 import { HeaderContainer } from './style'
+import { Button } from './style'
 
 export default function Header() {
+
+  const [screen, setScreen] = useState("/");
+
+    // getHeaderPage = () => {
+    //   switch(){
+    //     case "":
+    //       return 
+    //   }
+    // }
+
+    console.log(window.location.pathname)
+
   return (
     <HeaderContainer>
-        <img src={PokeLogo}></img>
+      <button></button>
+      <img src={PokeLogo}></img>
+      <Button>Pokédex</Button>
     </HeaderContainer>
   )
 }
